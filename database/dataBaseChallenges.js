57ace37608932b5c94a964b3cd0101cfe830c475
mahdiar <mahdiarrezvani59@gmail.com>
2023-09-16 10:05:20 -0700

    ds

--- database/dataBaseChallenges.js
+++ database/dataBaseChallenges.js
@@ -4,9 +4,9 @@ export const dataBase = {
     usefulWorks: [
         {
             infoChallenge: { start: '1402/06/18', end: '1402/06/27', period: 10, target: 3000 },
-            mahdiar: { name: 'mahdiar', state: '👎', result: 0, arrayChallengeProcess: [211, 183, 136, 186, 140] },
-            amin: { name: 'amin', state: '👎', result: 0, arrayChallengeProcess: [180, 237, 125, 150, 220] },
-            abbas: { name: 'abbas', state: '👎', result: 0, arrayChallengeProcess: [100, 144, 165, 50, 30] }
+            mahdiar: { name: 'mahdiar', state: '👎', result: 0, arrayChallengeProcess: [211, 183, 136, 186, 140, 150, 12] },
+            amin: { name: 'amin', state: '👎', result: 0, arrayChallengeProcess: [180, 237, 125, 150, 220, 0, 173] },
+            abbas: { name: 'abbas', state: '👎', result: 0, arrayChallengeProcess: [100, 144, 165, 50, 30, 0, 0] }
         },
     ],
 }
