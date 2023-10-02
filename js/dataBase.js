324e8b2166192afc30d366709be760738ca4f76b
mahdiar <mahdiarrezvani59@gmail.com>
2023-10-02 07:18:07 -0700

    asd

--- js/dataBase.js
+++ js/dataBase.js
@@ -1,5 +1,5 @@
 import { month1Mahdiar, month1Amin, month1Abbas } from '../database/month1.js'
-// import { month2Mahdiar, month2Amin, month2Abbas } from '../database/month2.js'
+import { month2Mahdiar, month2Amin, month2Abbas } from '../database/month2.js'
 // import { month3Mahdiar, month3Amin, month3Abbas } from '../database/month3.js'
 // import { month4Mahdiar, month4Amin, month4Abbas } from '../database/month4.js'
 // import { month5Mahdiar, month5Amin, month5Abbas } from '../database/month5.js'
@@ -8,7 +8,7 @@ export let dataBase = [
     // Mahdiar
     [
         ...month1Mahdiar,
-        // ...month2Mahdiar,
+        ...month2Mahdiar,
         // ...month3Mahdiar,
         // ...month4Mahdiar,
         // ...month5Mahdiar,
@@ -17,7 +17,7 @@ export let dataBase = [
     // Amin
     [
         ...month1Amin,
-        // ...month2Amin,
+        ...month2Amin,
         // ...month3Amin,
         // ...month4Amin,
         // ...month5Amin,
@@ -26,7 +26,7 @@ export let dataBase = [
     // Amir Abbas
     [
         ...month1Abbas,
-        // ...month2Abbas,
+        ...month2Abbas,
         // ...month3Abbas,
         // ...month4Abbas,
         // ...month5Abbas,
