0ad3ff360fadc88d42cd348c671994c4aed4b31e
mahdiar <mahdiarrezvani59@gmail.com>
2023-10-13 02:02:04 -0700

    finish

--- database/month4.js
+++
@@ -1,5 +0,0 @@
-let month4Mahdiar = [];
-let month4Amin = [];
-let month4Abbas = [];
-export { month4Mahdiar, month4Amin, month4Abbas };
-// { date: "1402/06/00", day: "day", code: "0", wasted: "0", study: "0", english: "0", learning: "0" },
--- database/month5.js
+++
@@ -1,5 +0,0 @@
-let month5Mahdiar = [];
-let month5Amin = [];
-let month5Abbas = [];
-export { month5Mahdiar, month5Amin, month5Abbas };
-// { date: "1402/06/00", day: "day", code: "0", wasted: "0", study: "0", english: "0", learning: "0" },
